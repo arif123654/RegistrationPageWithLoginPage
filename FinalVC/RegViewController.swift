ad1225671ba701172e2e81648a8ca7a1f9e757fc
Apple MacBook Pro <Apple@Apples-MacBook-Pro.local>
2020-03-05 12:10:33 +0600

    with post methode

--- FinalVC/RegViewController.swift
+++ FinalVC/RegViewController.swift
@@ -7,10 +7,25 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 class RegViewController: UIViewController {
-
+    
+    
+    
+    //MARK:- Utilities
+    @IBOutlet weak var firatNameTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var lastNameTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var emailTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var mobileTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var genderTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var locationTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var districtTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var passwordTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var confirmPasswordTextField: SkyFloatingLabelTextField!
     @IBOutlet weak var signUpButton: UIButton!
+    
+    //MARK:- Init
     override func viewDidLoad() {
         super.viewDidLoad()
         self.signUpButton.layer.cornerRadius = 7
@@ -18,25 +33,62 @@ class RegViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     
     
-    
+    //MARK:- Sign Up
     @IBAction func signUpButton(_ sender: Any) {
-        let vc = storyboard?.instantiateViewController(identifier: "ShowTableButtonViewController") as! ShowTableButtonViewController
-        
-        self.navigationController?.pushViewController(vc, animated: true)
+        if confirmPasswordTextField.text! == passwordTextField.text!{
+            postdata()
+        } else {
+            confirmPasswordTextField.errorMessage = "Password Doesn't match"
+        }
     }
     
     
-    
+    //MARK:- POST methode function
+    func postdata(){
+        let parameters = ["FirstName": firatNameTextField.text!,"LastName":lastNameTextField.text!,"mobile":mobileTextField.text!,"email":emailTextField.text!,"gender":genderTextField.text!,"district":districtTextField.text!,"location":locationTextField.text!, "password": passwordTextField.text!] as [String : Any]
+            //let parameters = ["mobile":"01742011644", "password": "123456"] as [String : Any]
+
+            //create the url with URL
+            let url = URL(string: "http://192.168.0.30/bdweather/API/registration.php")! //change the url
+
+            //create the session object
+            let session = URLSession.shared
+
+            //now create the URLRequest object using the url object
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST" //set http method as POST
+
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            } catch let error {
+                print(error.localizedDescription)
+            }
+
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+            //create dataTask using the session object to send data to the server
+            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+                guard error == nil else {   return  }
+
+                guard let data = data else {    return  }
+
+                do {
+                    print(data)
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+                } catch let error {
+                   
+                    print(error.localizedDescription)
+                }
+            })
+            task.resume()
+        }
 }
--- FinalVC/ViewController.swift
+++ FinalVC/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var mobileTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var passwordTextField: SkyFloatingLabelTextField!
     
+    
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +24,21 @@ class ViewController: UIViewController {
         self.signUpButton.layer.cornerRadius = 7
     }
 
+    func landingPage(){
+        let vc = storyboard?.instantiateViewController(identifier: "ShowTableButtonViewController") as! ShowTableButtonViewController
+               self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         
-        let vc = storyboard?.instantiateViewController(identifier: "ShowTableButtonViewController") as! ShowTableButtonViewController
+       
         
-        self.navigationController?.pushViewController(vc, animated: true)
+        if mobileTextField.text == nil{
+            mobileTextField.errorMessage = "Enter Your Mobile No"
+        } else if passwordTextField.text == nil {
+            passwordTextField.errorMessage = "Enter Your Password"
+        } //else if
+        
     }
     
     @IBAction func signUpButton(_ sender: Any) {
@@ -34,7 +48,53 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
         
     }
-    
+    //MARK:- POST methode function
+    func postdata(){
+        let parameters = ["mobile":mobileTextField.text!,"password":passwordTextField.text!] as [String:Any]
+            //let parameters = ["mobile":"01742011644", "password": "123456"] as [String : Any]
+
+            //create the url with URL
+            let url = URL(string: "http://192.168.0.30/bdweather/API/login.php")! //change the url
+
+            //create the session object
+            let session = URLSession.shared
+
+            //now create the URLRequest object using the url object
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST" //set http method as POST
+
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+            //create dataTask using the session object to send data to the server
+            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+                guard error == nil else {   return  }
+
+                guard let data = data else {    return  }
+
+                do {
+                    print(data)
+                  
+                    
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+                } catch let error {
+                   
+                    print(error.localizedDescription)
+                }
+            })
+            task.resume()
+        }
     
     
 }
